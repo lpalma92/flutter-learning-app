a2716ccc22e681c3f267f2bf8407f65feaf77c4d
Enrique Palma <lpalma.dev@gmail.com>
2021-06-28 22:56:46 -0500

    Settings for publication on stores

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Learning App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -19,10 +19,11 @@ class MyApp extends StatelessWidget {
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        // is not restarted.      
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Learning App'),
     );
   }
 }
